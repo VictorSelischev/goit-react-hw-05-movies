f9a041efe6166cb04283ff553aac424a3b7c587d
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-11-10 19:01:20 +0200

    Create Render Reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { getMovieReviews } from 'services/fetchAPI';
 import { useEffect, useState } from 'react';
+import { ListItem, Paragraph, SectionTitle } from './Reviews.styled';
 
 const Reviews = () => {
   const [reviews, setReviews] = useState([]);
@@ -8,14 +9,27 @@ const Reviews = () => {
 
   useEffect(() => {
     getMovieReviews(movieId)
-        .then(data => {
-            console.log(data)
-            setReviews(data.results);
-        })
+      .then(data => {
+        console.log(data);
+        setReviews(data.results);
+      })
       .catch(error => console.log(error));
   }, [movieId]);
 
-  // return ();
+  return (
+    <ul>
+      {reviews.length === 0 ? (
+        <p>We don't have any reviews for this movie</p>
+      ) : (
+        reviews.map(review => (
+          <ListItem key={review.id}>
+            <SectionTitle>Autor: {review.author}</SectionTitle>
+            <Paragraph>{review.content}</Paragraph>
+          </ListItem>
+        ))
+      )}
+    </ul>
+  );
 };
 
 export default Reviews;
---
+++ src/components/Reviews/Reviews.styled.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ListItem = styled.li`
+  margin-bottom: 28px;
+`;
+
+export const SectionTitle = styled.h4`
+  margin-bottom: 16px;
+`;
+
+export const Paragraph = styled.p`
+  line-height: 1.4;
+`;
