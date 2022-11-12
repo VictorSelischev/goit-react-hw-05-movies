942f32fb98fb70ee602ece12733a584b9066623c
VictorSelischev <95778687+VictorSelischev@users.noreply.github.com>
2022-11-12 09:25:58 +0000

    Deploying to gh-pages from @ VictorSelischev/goit-react-hw-05-movies@fb3a18d39101f8ce7ca06307c54d7fba5d4ecd87 🚀

---
+++ static/js/71.74d1211f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[71],{71:function(t,e,c){c.d(e,{Y1g:function(){return i}});var a=c(983);function i(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M464 440l-28.12-32.11c-22.48-25.65-43.33-45.45-72.08-58.7-26.61-12.26-60-18.65-104.27-19.84V432L48 252 259.53 72v103.21c72.88 3 127.18 27.08 161.56 71.75C449.56 284 464 335.19 464 399.26z"}}]})(t)}}}]);
+//# sourceMappingURL=71.74d1211f.chunk.js.map
